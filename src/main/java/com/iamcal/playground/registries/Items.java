f035e66f2c26e8e043f3d51a75e6026a059cfc97
Cal Henderson <cal@iamcal.com>
2024-07-21 15:27:36 -0700

    the suspicious substance is now a poisonous snack

--- src/main/java/com/iamcal/playground/registries/Items.java
+++ src/main/java/com/iamcal/playground/registries/Items.java
@@ -3,6 +3,9 @@ package com.iamcal.playground.registries;
 import com.iamcal.playground.items.SuspiciousSubstance;
 import net.fabricmc.fabric.api.registry.CompostingChanceRegistry;
 import net.fabricmc.fabric.api.registry.FuelRegistry;
+import net.minecraft.component.type.FoodComponent;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
@@ -15,8 +18,15 @@ import com.iamcal.playground.Playground;
 
 public class Items {
 
+    public static final FoodComponent SUSPICIOUS_FOOD_COMPONENT = new FoodComponent.Builder()
+            .alwaysEdible()
+            .snack()
+            // The duration is in ticks, 20 ticks = 1 second
+            .statusEffect(new StatusEffectInstance(StatusEffects.POISON, 6 * 20, 1), 1.0f)
+            .build();
+
     public static final Item SUSPICIOUS_SUBSTANCE = register(
-            new SuspiciousSubstance(new Item.Settings()), "suspicious_substance"
+            new SuspiciousSubstance(new Item.Settings().food(SUSPICIOUS_FOOD_COMPONENT)), "suspicious_substance"
     );
 
     public static Item register(Item item, String id) {
